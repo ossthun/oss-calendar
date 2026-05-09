e65f0ef8a95468ef1e0e3685ad97ab4c0c344f8b
ossthun <msline3@bluewin.ch>
2026-05-09 19:58:48 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Calendar is loading...</h1>
+      <p>Next step: we will build the full calendar system.</p>
+    </div>
+  );
+}
