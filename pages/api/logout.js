140fefab00a8f3a9b7097fc318ed97e60e835dd3
ossthun <msline3@bluewin.ch>
2026-05-13 16:12:19 +0200

    Create logout.js

---
+++ pages/api/logout.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  res.setHeader(
+    "Set-Cookie",
+    "admin=; HttpOnly; Path=/; SameSite=Lax; Max-Age=0"
+  );
+
+  return res.status(200).json({
+    success: true,
+  });
+}
