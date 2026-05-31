9cd3041f0ca4c97f88ac3aaf8c66eb5b1104af65
ossthun <msline3@bluewin.ch>
2026-05-31 19:43:43 +0200

    Create custom Document component for Next.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="icon"
+          href="/icon.png"
+          type="image/png"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
